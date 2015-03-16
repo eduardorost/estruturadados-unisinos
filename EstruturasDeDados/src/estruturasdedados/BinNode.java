372aa7c6df66f59e996fc4198e5f1cad2030bbfe
Fabiano <Fabiano@192.168.25.127>
2015-03-15 21:28:26 -0300

    ADT para árvore binária - Interface BinNode

---
+++ EstruturasDeDados/src/estruturasdedados/BinNode.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estruturasdedados;
+/**
+ *
+ * @author Fabiano
+ */
+/** ADT for binary tree nodes
+ * @param <E> */
+public interface BinNode<E> {
+    /**get e set
+     * @return  */
+    public E element();
+    public E setElement(E v);
+    
+    /** Retorna o filho da esquerda
+     * @return  */
+    public BinNode<E> left();
+    
+    /** Retorna o filho da direita
+     * @return  */
+    public BinNode<E> right();
+    
+    /** Retorna true se é um folha
+     * @return  */
+    public boolean isLeaf();    
+}
