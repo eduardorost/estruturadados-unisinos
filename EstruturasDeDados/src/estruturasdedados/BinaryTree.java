86b15194b7868657803b30aa8e76b80ed8f42bcb
Fabiano <Fabiano@192.168.25.127>
2015-03-15 22:10:56 -0300

    Classe BinaryTree

---
+++ EstruturasDeDados/src/estruturasdedados/BinaryTree.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estruturasdedados;
+/**
+ *
+ * @author Fabiano
+ */
+public class BinaryTree  {
+    
+    public BinaryTree(){   
+    }
+    
+    /** @param rt raiz da subtree */
+    public void preOrder(BinNode rt){
+        if ( rt != null){            
+            visit(rt);
+            preOrder(rt.left());
+            preOrder(rt.right());
+        }
+    }
+    
+    /** @param rt raiz da subtree */
+    public void inOrder(BinNode rt){
+        if (rt != null){
+            inOrder(rt.left());
+            visit(rt);
+            inOrder(rt.right());
+        }
+    }
+    
+    /** @param rt raiz da subtree */
+    public void postOrder(BinNode rt){
+        if  (rt != null){
+            postOrder(rt.left());
+            postOrder(rt.right());
+            visit(rt);
+        }
+    }
+    
+    /** @param rt raiz da subtree */
+    public void visit(BinNode rt){
+        System.out.println("Nodo " + rt.toString() + " visitado!!");
+    }
+}
