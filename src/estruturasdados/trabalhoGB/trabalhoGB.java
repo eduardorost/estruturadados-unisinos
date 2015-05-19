5e0129aedac2f6519bba5d09232bc35ebe7fad10
Eduardo Rost <eduardo.a.rost@gmail.com>
2015-05-18 22:39:08 -0300

    Revert "Iniciando trabalho GB"
    This reverts commit 04d06d5c9b6cc70081a9cfaa0f753d9f028485a6.

--- src/estruturasdados/trabalhoGB/trabalhoGB.java
+++ src/estruturasdados/trabalhoGB/trabalhoGB.java
@@ -5,33 +5,12 @@
  */
 package estruturasdados.trabalhoGB;
 
-import estruturasdados.trabalhoGB.Helpers.FileHelper;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 /**
  *
  * @author rosted
  */
 public class trabalhoGB {
     public static void main(String[] args) {
-        FileHelper fh = new FileHelper();
-        
-        File input = fh.getFile();
-        List<String> lines = null;
-        
-        try {
-            lines = Files.readAllLines(Paths.get(input.getAbsolutePath()));
-        } catch (IOException ex) {
-            Logger.getLogger(trabalhoGB.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        for (String line : lines) {
-            System.out.println(line);            
-        }
+    
     }
 }
