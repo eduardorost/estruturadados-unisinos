ef0492d2495dc2c3c96d9ebf5214a0bbd5e5c7ca
Eduardo Rost <eduardo.a.rost@gmail.com>
2015-05-18 23:11:56 -0300

    ITEM 1 - Leitura do arquivo

--- src/estruturasdados/trabalhoGB/trabalhoGB.java
+++ src/estruturasdados/trabalhoGB/trabalhoGB.java
@@ -13,25 +13,37 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 /**
  *
  * @author rosted
  */
 public class trabalhoGB {
+
     public static void main(String[] args) {
         FileHelper fh = new FileHelper();
-        
+
         File input = fh.getFile();
         List<String> lines = null;
-        
-        try {
-            lines = Files.readAllLines(Paths.get(input.getAbsolutePath()));
-        } catch (IOException ex) {
-            Logger.getLogger(trabalhoGB.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
+        boolean validFile = false;
+
+        do {
+            try {
+                lines = Files.readAllLines(Paths.get(input.getAbsolutePath()));
+            } catch (IOException ex) {
+                Logger.getLogger(trabalhoGB.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            if ("P3".equals(lines.remove(0).toUpperCase())) {
+                validFile = true;
+            } else {
+                System.out.println("Arquivo incorreto! Tente novamente.");
+                input = fh.getFile();
+            }
+        } while (!validFile);
+
         for (String line : lines) {
-            System.out.println(line);            
+            System.out.println(line);
         }
     }
 }
