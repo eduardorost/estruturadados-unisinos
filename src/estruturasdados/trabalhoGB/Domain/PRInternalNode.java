2f91009d18b849ea01dca246ab9244cf27250bf6
Eduardo Rost <eduardo.a.rost@gmail.com>
2015-05-22 14:14:57 -0300

    ITEM 4 - Corrigindo problema na posição do pixel no quadrante

--- src/estruturasdados/trabalhoGB/Domain/PRInternalNode.java
+++ src/estruturasdados/trabalhoGB/Domain/PRInternalNode.java
@@ -48,13 +48,14 @@ public class PRInternalNode extends PRNode implements Serializable {
             case NW:
                 if (nwChild == null) {
                     nwChild = node;
+                    node.position = PositionEnum.NW;
                 } else {
                     if (nwChild instanceof PRInternalNode) {
                         ((PRInternalNode) nwChild).insert(node);
                     } else {
                         //EXEMPLOS ESTÃO TODOS DIVISIVEIS POR 2.
-                        int[] w = {this.marginW[0], this.marginW[1] / 2};
-                        int[] h = {this.marginH[0], this.marginH[1] / 2};
+                        int[] w = {this.marginW[0], Math.floorDiv(this.marginW[1], 2)};
+                        int[] h = {this.marginH[0], Math.floorDiv(this.marginH[1], 2)};
 
                         PRInternalNode internalNode = new PRInternalNode(this.width / 2, this.height / 2, w, h, PositionEnum.NW, this);
                         internalNode.insert((PRLeafNode) nwChild);
@@ -66,13 +67,14 @@ public class PRInternalNode extends PRNode implements Serializable {
             case NE:
                 if (neChild == null) {
                     neChild = node;
+                    node.position = PositionEnum.NE;
                 } else {
                     if (neChild instanceof PRInternalNode) {
                         ((PRInternalNode) neChild).insert(node);
                     } else {
                         //EXEMPLOS ESTÃO TODOS DIVISIVEIS POR 2.
-                        int[] w = {this.marginW[1] / 2, this.marginW[1]};
-                        int[] h = {this.marginH[0], this.marginH[1] / 2};
+                        int[] w = {Math.floorDiv(this.marginW[1], 2) + 1, this.marginW[1]};
+                        int[] h = {this.marginH[0], Math.floorDiv(this.marginH[1], 2)};
 
                         PRInternalNode internalNode = new PRInternalNode(this.width / 2, this.height / 2, w, h, PositionEnum.NE, this);
                         internalNode.insert((PRLeafNode) neChild);
@@ -84,13 +86,14 @@ public class PRInternalNode extends PRNode implements Serializable {
             case SW:
                 if (swChild == null) {
                     swChild = node;
+                    node.position = PositionEnum.SW;
                 } else {
                     if (swChild instanceof PRInternalNode) {
                         ((PRInternalNode) swChild).insert(node);
                     } else {
                         //EXEMPLOS ESTÃO TODOS DIVISIVEIS POR 2.
-                        int[] w = {this.marginW[0], this.marginW[1] / 2};
-                        int[] h = {this.marginH[1] / 2, this.marginH[1]};
+                        int[] w = {this.marginW[0], Math.floorDiv(this.marginW[1], 2)};
+                        int[] h = {Math.floorDiv(this.marginH[1], 2) + 1, this.marginH[1]};
 
                         PRInternalNode internalNode = new PRInternalNode(this.width / 2, this.height / 2, w, h, PositionEnum.SW, this);
                         internalNode.insert((PRLeafNode) swChild);
@@ -102,13 +105,14 @@ public class PRInternalNode extends PRNode implements Serializable {
             case SE:
                 if (seChild == null) {
                     seChild = node;
+                    node.position = PositionEnum.SE;
                 } else {
                     if (seChild instanceof PRInternalNode) {
                         ((PRInternalNode) seChild).insert(node);
                     } else {
                         //EXEMPLOS ESTÃO TODOS DIVISIVEIS POR 2.
-                        int[] w = {this.marginW[1] / 2, this.marginW[1]};
-                        int[] h = {this.marginH[1] / 2, this.marginH[1]};
+                        int[] w = {Math.floorDiv(this.marginW[1], 2) + 1, this.marginW[1]};
+                        int[] h = {Math.floorDiv(this.marginH[1], 2) + 1, this.marginH[1]};
 
                         PRInternalNode internalNode = new PRInternalNode(this.width / 2, this.height / 2, w, h, PositionEnum.SE, this);
                         internalNode.insert((PRLeafNode) seChild);
